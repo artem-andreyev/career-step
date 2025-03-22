0cd7f62c7617c70b028ca4f82bd307c0db347c79
Artem <artem.andreyev212@gmail.com>
2025-03-22 22:52:26 +0200

    Working on a website, adding tailwind

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,8 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Career Step</title>
+    @vite(['resources/js/app.js'])
 </head>
 <body>
+
+    <div>
+        <nav>
+            <div>
+                <a href="/">
+                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
+                </a>
+            </div>
+
+            <div>
+                <a href="#">Jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salaries</a>
+                <a href="#">Companies</a>
+            </div>
+
+            <div>
+                <a href="">Post a Job</a>
+            </div>
+        </nav>
+
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
 
 </body>
 </html>
